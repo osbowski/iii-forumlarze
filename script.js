35b18390caaf2774b90bb1bf4c9cf888bf5171e2
osbowski <osobiesam@gmail.com>
2021-12-06 11:26:06 +0100

    save variables with dom elements

---
+++ script.js
@@ -0,0 +1,4 @@
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const priorityInput = document.querySelector("#read-priority");
+const categoryInput = document.querySelector("#category");
